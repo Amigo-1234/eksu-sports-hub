af3139580119301182150f58c2a861ec61d1e157
Amigo-1234 <olamidesamson841@gmail.com>
2026-03-08 17:21:51 +0100

    sport inteface done

--- match.js
+++ match.js
@@ -5,10 +5,11 @@ const away = params.get("away");
 const hs = params.get("hs");
 const as = params.get("as");
 
-document.getElementById("matchTitle").innerText = `${home} vs ${away}`;
-
 document.getElementById("homeTeam").innerText = home;
-
 document.getElementById("awayTeam").innerText = away;
 
 document.getElementById("score").innerText = `${hs} - ${as}`;
+
+document.getElementById("competition").innerText = "Inter Department Match";
+
+document.getElementById("matchTime").innerText = "16:00";
